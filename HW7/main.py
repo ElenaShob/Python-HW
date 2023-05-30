1837f274812782660b64d6466366e7a1a17b0989
MAC <mac@MacBook-Pro-MAC.local>
2023-05-30 19:28:00 +0300

    HW 7 done 1

--- HW7/main.py
+++ HW7/main.py
@@ -51,15 +51,14 @@
 5 10 15 20 25 30
 6 12 18 24 30 36"""
 
-def print_operation_table(operation, num_rows, num_columns):
+# def print_operation_table(operation, num_rows, num_columns):
         
-    for i in range(1, num_columns + 1):
-        list1 = list()
-        for j in range(1, num_rows + 1):
-            list1.append(operation(i, j))
-        print(*list1)
+#     for i in range(1, num_columns + 1):
+#         list1 = list()
+#         for j in range(1, num_rows + 1):
+#             list1.append(operation(i, j))
+#         print(*list1)
    
-print_operation_table(lambda x, y: x * y, 6, 6)
+# print_operation_table(lambda x, y: x * y, 6, 6)
 
-"""
-Если нумерацию вести с нуля первый столбец и """
+
